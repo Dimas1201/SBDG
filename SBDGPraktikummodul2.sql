c43b8b0dd8a0383a45f279eea15a9b5a3c336d4a
Dimas1201 <144188840+Dimas1201@users.noreply.github.com>
2023-11-11 19:40:54 +0700

    Add files via upload
    Revisi

--- SBDGPraktikummodul2.sql
+++ SBDGPraktikummodul2.sql
@@ -55,6 +55,7 @@ CREATE TABLE mekanik(
 	mk_nama VARCHAR(60)
 )
 
+-- no. 2
 CREATE TABLE supplier(
 	sp_id CHAR (6) PRIMARY KEY,
 	sp_nama VARCHAR (60),
@@ -62,7 +63,7 @@ CREATE TABLE supplier(
 	sp_email VARCHAR (256),
 	sp_alamat VARCHAR (100)
 )
-
+-- No. 3
 RENAME TABLE mekanik TO pegawai
 
 CREATE TABLE posisi(
@@ -71,50 +72,156 @@ CREATE TABLE posisi(
 )
 
 ALTER TABLE pegawai
-ADD CONSTRAINT ps_mk_id FOREIGN KEY (ps_mk_id) REFERENCES posisi(ps_id) ON DELETE CASCADE;
+ADD COLUMN ps_id CHAR(6) NOT NULL,
+ADD CONSTRAINT fk_posisi FOREIGN KEY (ps_id) REFERENCES posisi(ps_id);
 
+-- No. 4
 DROP TABLE posisi
 
 ALTER TABLE pegawai
 ADD COLUMN mk_posisi VARCHAR(15);
 
-INSERT INTO pelanggan (p_id, p_nama, p_no_telp, p_email, p_alamat) VALUES
+-- No. 5
+-- Masukkan data pada tabel pelanggan
+INSERT INTO pelanggan (p_id, p_nama, p_no_telp, p_email, p_alamat)
+VALUES 
 ('3225011201880002', 'Andy Williams', '62123456789', 'andy@gmail.com', 'Jl. Apel no 1'),
-('3525010706950001', 'Marshall Paul', '621451871011', 'paulan@gmail.com', 'Jl. Jeruk no 12',),
+('3525010706950001', 'Marshall Paul', '621451871011', 'paulan@gmail.com', 'Jl. Jeruk no 12'),
 ('3525016005920002', 'Kazuya Tanaka', '62190129190', 'tanaka@gmail.com', 'Jl. JKT no 48'),
 ('3975311107780001', 'Budi Prutomo', '621545458901', 'NULL', 'Jl. Nanas no 45'),
 ('3098762307810002', 'Razai Ambudi', '621898989102', 'NULL', 'Jl. Mangga no 2');
 
-INSERT INTO mobil (mb_vin, mb_merk, mb_tipe, mb_tahun, mb_warna, mb_p_id) VALUES
+-- Masukkan data pada tabel mobil
+INSERT INTO mobil (mb_vin, mb_merk, mb_tipe, mb_tahun, mb_warna, mb_p_id)
+VALUES 
 ('JN8AZ1MW4BW678706', 'Nissan', 'Murano', '2011', 'biru', '3525010706950001'),
 ('2LMTJ8JP0GSJ00175', 'Lincoln', 'MKX', '2016', 'merah', '3525016005920002'),
 ('ZFF76ZHT3E0201920', 'Ferrari', 'Ferrari', '2014', 'merah', '3975311107780001'),
 ('1HGCP26359A157554', 'Honda', 'Accord', '2009', 'hitam', '3225011201880002'),
 ('5YJSA1DN5CFP01785', 'Tesla', 'Model S', '2012', 'putih', '3098762307810002');
 
-INSERT INTO pegawai (mk_id, mk_nama, mk_posisi) VALUES
+-- Masukkan data pada tabel pegawai
+INSERT INTO pegawai (mk_id, mk_nama, mk_posisi)
+VALUES 
 ('MK0001', 'Walter Jones', 'mekanik'),
 ('MK0002', 'Kentaki Veraid', 'kasir'),
 ('MK0003', 'Leo', 'mekanik'),
-('MK0004', 'Reyhand', 'Janita pencuci'),
+('MK0004', 'Reyhand Janita', 'pencuci'),
 ('MK0005', 'Elizabeth Alexandra', 'mekanik');
 
-INSERT INTO supplier (sp_id, sp_nama, sp_no_telp, sp_email, sp_alamat) VALUES
+-- Masukkan data pada tabel supplier
+INSERT INTO supplier (sp_id, sp_nama, sp_no_telp, sp_email, sp_alamat)
+VALUES 
 ('SP0001', 'Indotrading', '6282283741247', 'indotrading@rocketmail.com', 'Jl. Bambu no 5'),
 ('SP0002', 'Jayasinda', '628227428238', 'Jayasinda@yahoo.com', 'Jl. Padi no 12'),
 ('SP0003', 'SAS Autoparts', '6282212382311', 'sasparts@gmail.com', 'Jl. Sorghum no 24');
 
-INSERT INTO suku_cadang (sc_id, sc_nama, sc_deskripsi, sc_harga) VALUES
+-- Masukkan data pada tabel suku_cadang
+INSERT INTO suku_cadang (sc_id, sc_nama, sc_deskripsi, sc_harga)
+VALUES 
 ('SC00001', 'Damper', 'Damper Per Belakang Original', '800000.00'),
-('SC00002','Coil Ignition','Coil Ignition Denso Jepang','550000.00'),
-('SC00003','Selang Filter','Selang Filter Udara Original','560000.00'),
-('SC00004','Bushing','Bushing Upper Arm Original','345000.00'),
-('SC00005','Radiator Racing','Radiator Racing Kotorad Manual Diesel','6750000.00');
+('SC00002', 'Coil Ignition', 'Coil Ignition Denso Jepang', '550000.00'),
+('SC00003', 'Selang Filter', 'Selang Filter Udara Original', '560000.00'),
+('SC00004', 'Bushing', 'Bushing Upper Arm Original', '345000.00'),
+('SC00005', 'Radiator Racing', 'Radiator Racing Kotorad Manual Diesel', '6750000.00');
 
+-- Masukkan data pada tabel tiket_servis
 INSERT INTO tiket_servis (ts_id, ts_waktu_masuk, ts_waktu_keluar, ts_deskripsi, ts_komentar, ts_p_id, ts_mb_vin)
-VALUES
-('','','','','','',''),
-('','','','','','',''),
-('','','','','','',''),
-('','','','','','',''),
-('','','','','','','');
+VALUES 
+('TS000001', '2023-11-05 08:00:00', '2023-11-05 16:30:00', 'Mobil mengalami getaran yang tidak normal saat berkendara. Untuk memperbaikinya, kami akan mengganti damper yang aus dan juga bushing yang telah rusak.', 'Saya sangat puas dengan perbaikan ini. Mobil saya sekarang berjalan lebih halus dan getarannya hilang. Terima kasih atas pelayanan yang baik!', '3098762307810002', '5YJSA1DN5CFP01785'),
+('TS000002', '2023-11-05 09:15:00', '2023-11-05 17:45:00', 'Mesin mobil sering mati secara tiba-tiba atau tidak berjalan dengan baik. Permasalahannya terletak pada coil ignition yang bermasalah. Kami akan menggantinya dan melakukan pengaturan ulang untuk memastikan kinerja mesin yang lebih baik.', 'Mesin mobil saya sekarang berjalan seperti baru lagi. Tidak ada lagi mati mendadak, dan kinerjanya sangat baik. Pelayanan yang cepat dan profesional.', '3975311107780001', 'ZFF76ZHT3E0201920'),
+('TS000003', '2023-11-06 10:30:00', '2023-11-06 18:15:00', 'Untuk meningkatkan kinerja pendinginan mesin, kami akan membersihkan radiator dan memeriksa apakah ada kerusakan atau kebocoran. Jika diperlukan, kami akan mengganti komponen yang rusak.', 'Mobil saya sekarang tidak lagi mengalami masalah panas berlebihan. Radiator berfungsi dengan baik, dan saya merasa aman saat berkendara. Terima kasih atas perbaikan yang berkualitas', '3525016005920002', '2LMTJ8JP0GSJ00175'),
+('TS000004', '2023-11-06 11:45:00', '2023-11-06 19:30:00', 'Mobil mengalami masalah saat memasok bahan bakar ke mesin. Masalah ini terjadi karena selang filter bahan bakar yang tersumbat. Kami akan menggantinya sehingga aliran bahan bakar menjadi lancar kembali.', 'Setelah penggantian selang filter bahan bakar, mobil saya kembali berjalan dengan lancar. Tidak ada lagi masalah pengiriman bahan bakar. Pelayanan yang sangat membantu', '3525010706950001', 'JN8AZ1MW4BW678706'),
+('TS000005', '2023-11-06 12:30:00', '2023-11-06 20:00:00', 'Mobil akan menjalani pemeliharaan komprehensif, termasuk pemeriksaan seluruh sistem. Ini termasuk penggantian damper yang aus, coil ignition yang bermasalah, serta pemeriksaan dan perawatan selang filter dan radiator racing.', 'Semua suku cadang yang perlu diganti akan diperbaiki agar mobil berfungsi seperti semula. Pemeliharaan komprehensif yang telah dilakukan membuat mobil saya seperti baru. Semua masalah telah diperbaiki, dan saya merasa yakin dengan kualitas perbaikan yang diberikan. Terima kasih atas kerja kerasnya', '3225011201880002', '1HGCP26359A157554');
+
+-- Masukkan data pada tabel mekanik_servis
+INSERT INTO mekanik_servis (mksv_ts_id, mksv_mk_id)
+VALUES 
+('TS000001', 'MK0005'),
+('TS000002', 'MK0003'),
+('TS000002', 'MK0004'),
+('TS000003', 'MK0005'),
+('TS000004', 'MK0003'),
+('TS000005', 'MK0001'),
+('TS000005', 'MK0005'),
+('TS000005', 'MK0004');
+
+-- Masukkan data pada tabel suku_cadang_servis
+INSERT INTO suku_cadang_servis (scsv_ts_id, scsv_sc_id)
+VALUES 
+('TS000001', 'SC00001'),
+('TS000001', 'SC00004'),
+('TS000002', 'SC00002'),
+('TS000003', 'SC00005'),
+('TS000004', 'SC00003'),
+('TS000005', 'SC00001'),
+('TS000005', 'SC00002'),
+('TS000005', 'SC00003'),
+('TS000005', 'SC00005');
+
+
+-- NO 6
+UPDATE tiket_servis
+SET ts_waktu_keluar = CURRENT_TIMESTAMP;
+
+SELECT * FROM tiket_servis;
+
+-- NO 7
+UPDATE pelanggan
+SET p_nama ='Andi Williams', p_email ='andi@bing.com', p_alamat = 'Jl. Koreka no 15'
+WHERE p_id = '3225011201880002';
+
+SELECT * FROM pelanggan;
+
+-- NO 8
+ALTER TABLE suku_cadang
+ADD COLUMN sc_sp_id CHAR(6);
+
+ALTER TABLE suku_cadang
+ADD CONSTRAINT fk_supplier FOREIGN KEY (sc_sp_id) REFERENCES supplier(sp_id);
+
+UPDATE suku_cadang
+SET sc_sp_id = 'SP0001'
+WHERE sc_id = 'SC00004';
+
+UPDATE suku_cadang
+SET sc_sp_id = 'SP0002'
+WHERE sc_id IN ('SC00001', 'SC00003');
+
+UPDATE suku_cadang
+SET sc_sp_id = 'SP0003'
+WHERE sc_id IN ('SC00002', 'SC00005');
+
+SELECT * FROM suku_cadang;
+
+-- NO 9
+UPDATE suku_cadang
+SET sc_harga = sc_harga * 1.10
+WHERE sc_harga > 500000;
+
+SELECT * FROM suku_cadang;
+
+-- NO 10
+INSERT INTO tiket_servis (ts_id, ts_waktu_masuk, ts_deskripsi, ts_p_id, ts_mb_vin)
+VALUES ('TS000006', CURRENT_TIMESTAMP, 'Mobil mengalami kerusakan damper belakang. Terdapat kebocoran cairan hidrolik dan kondisi damper sudah aus. Kami akan mengganti damper belakang.', '3525016005920002', '2LMTJ8JP0GSJ00175');
+
+INSERT INTO suku_cadang_servis (scsv_ts_id, scsv_sc_id)
+VALUES ('TS000006', 'SC00001');
+
+SELECT * FROM tiket_servis;
+SELECT * FROM suku_cadang_servis;
+
+-- NO 11
+UPDATE tiket_servis
+SET ts_waktu_keluar = CURRENT_TIMESTAMP, ts_komentar = 'Pelayanan kurang memuaskan, goresan pada mobil tidak diperhatikan.'
+WHERE ts_id = 'TS000006';
+
+DELETE FROM pegawai
+WHERE mk_id = 'MK0001';
+
+SELECT * FROM tiket_servis;
+SELECT * FROM pegawai;
+
+-- NO 12
+DELETE FROM mekanik_servis;
+SELECT * FROM mekanik_servis;
